eeb6482a581bc99a9d0d88e1a8d72e36de87fe25
Manas <manasmadan08@gmail.com>
2021-09-15 23:17:56 +0530

    Fixes

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT;
 
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-}
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
